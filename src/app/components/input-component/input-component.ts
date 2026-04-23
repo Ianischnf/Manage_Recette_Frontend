6f87fb134c04d16d574c4b9cdc5a949068dfe396
Ianischnf <81810701+Ianischnf@users.noreply.github.com>
2026-04-23 18:11:24 +0200

    Implémentation Reactive Form pour le formulaire d'inscription

--- src/app/components/input-component/input-component.ts
+++ src/app/components/input-component/input-component.ts
@@ -1,17 +1,77 @@
-import { Component, Input } from '@angular/core';
+import { Component, forwardRef, Input } from '@angular/core';
 import { NgClass } from '@angular/common';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-input-component',
   imports: [NgClass],
   templateUrl: './input-component.html',
   styleUrl: './input-component.css',
+
+  //  Permet à Angular de reconnaître ce composant comme un input de formulaire
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputComponent),
+      multi: true
+    }
+  ]
 })
-export class InputComponent {
+export class InputComponent implements ControlValueAccessor {
 
-
-  @Input() type : 'text' | 'email' | 'password' = 'text';
-  @Input() input : 'input' | undefined
+  //  Inputs classiques
+  @Input() type: 'text' | 'email' | 'password' = 'text';
+  @Input() input: 'input' | undefined;
   @Input() placeholder: string = '';
 
+  //  Valeur interne du champ
+  value = '';
+
+  //  État disabled
+  disabled = false;
+
+  //  Fonctions que Angular va nous donner (callbacks)
+  private onChange: (value: string) => void = () => {};
+  private onTouched: () => void = () => {};
+
+  //  Angular appelle ça pour injecter une valeur dans mon input
+  writeValue(value: string): void {
+    this.value = value ?? '';
+  }
+
+  //  Angular donne une fonction à appeler quand la valeur change
+  // sert à recevoir une fonction et la stocker (onChange)
+  registerOnChange(fn: (value: string) => void): void {
+    this.onChange = fn;
+  }
+
+  /*
+  fn = (value) => {
+  formControl.setValue(value);
+}
+
+*/
+
+  // Angular donne une fonction à appeler quand le champ est touché
+  registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
+
+  // Angular peut désactiver l'input
+  setDisabledState(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
+
+  //  Quand l’utilisateur tape dans l’input
+  onInput(event: Event): void {
+    const target = event.target as HTMLInputElement;
+
+    this.value = target.value;   // mise à jour locale
+    this.onChange(this.value);   // 🚀 informer Angular
+  }
+
+  // Quand l’utilisateur quitte le champ
+  onBlur(): void {
+    this.onTouched(); // 🚀 informer Angular que le champ a été touché
+  }
 }
--- src/app/pages/register-page/register-page.ts
+++ src/app/pages/register-page/register-page.ts
@@ -1,31 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { InputComponent } from "../../components/input-component/input-component";
 import { Button } from "../../components/button/button";
-import { UserRequest, UserService } from '../../services/user.service';
+import { UserRequest, UserService, } from '../../services/user.service';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register-page',
-  imports: [InputComponent, Button],
+  imports: [InputComponent, Button, ReactiveFormsModule],
   templateUrl: './register-page.html',
   styleUrl: './register-page.css',
 })
 export class RegisterPage {
 
-  form: UserRequest = {
-    firstName: '',
-    lastName: '',
-    email: '',
-    password: ''
-  }
+  private fb = inject(FormBuilder);
 
-  constructor(private userService : UserService) {}
+  RegisterForm = this.fb.nonNullable.group({
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]]
+  });
+
+
+
+  constructor(
+    private userService: UserService,
+  ) { }
 
   onCreateUser() {
-    this.userService.createUser(this.form).subscribe({
+
+    if (this.RegisterForm.invalid) {
+      this.RegisterForm.markAllAsTouched();
+    }
+
+    const user: UserRequest = this.RegisterForm.getRawValue();
+
+    this.userService.createUser(user).subscribe({
       next: (res) => {
         console.log("Inscription réussi");
       },
-
       error: (err) => {
         console.log("Erreur lors de l'inscription", err);
       }
