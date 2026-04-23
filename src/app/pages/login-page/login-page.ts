a75673b10ef65e9dbb542290255e7eb55dae5dee
Ianischnf <81810701+Ianischnf@users.noreply.github.com>
2026-04-23 21:20:11 +0200

    modif css

--- src/app/pages/login-page/login-page.ts
+++ src/app/pages/login-page/login-page.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Button } from "../../components/button/button";
 import { RouterLink } from "@angular/router";
 import { InputComponent } from '../../components/input-component/input-component';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-login-page',
-  imports: [Button, RouterLink, InputComponent],
+  imports: [Button, RouterLink, InputComponent, ReactiveFormsModule],
   standalone: true,
   templateUrl: './login-page.html',
   styleUrl: './login-page.css',
 })
-export class LoginPage {}
+export class LoginPage {
+
+  private fb = inject(FormBuilder);
+
+  LoginForm = this.fb.nonNullable.group({
+    email: ['', Validators.required],
+    password: ['', [Validators.required, Validators.minLength(6)]]
+  });
+
+  constructor(private userService : UserService) {}
+
+}
